dd7427e0f29b99e56dd30be379eafa02dc9847a3
Iustin Esanu <iustin.esanu7@gmail.com>
2024-01-23 08:37:10 +0200

    review model

---
+++ backend/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Models;
+
+public class Review
+{
+    public int? MovieId { get; set; }
+    public Movie? Movie { get; set; }
+
+    public string Comment { get; set; } = string.Empty;
+    public int Rating { get; set; }
+    public DateTime TimpCreare { get; set; }
+}
