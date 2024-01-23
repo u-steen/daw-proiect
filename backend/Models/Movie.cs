2f15e40a5887bca3d1d79ace489043c64ca230ef
Iustin Esanu <iustin.esanu7@gmail.com>
2024-01-23 08:34:17 +0200

    movie model

---
+++ backend/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Titlu { get; set; } = string.Empty;
+    public string Director { get; set; } = string.Empty;
+    public int An { get; set; }
+    // Many to One
+    public List<Review> Reviews { get; set; }
+    // Many to Many
+    public List<Categorie> Categorii { get; set; }
+}
