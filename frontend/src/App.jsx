687c9d76ee81e1e541dedd6322efeb1fdbe476a9
Iustin <iustin.esanu7@gmail.com>
2024-02-01 14:15:34 +0200

    main page react + integration with api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,62 @@
-const moviesJSON = [
-    {
-        id: 1,
-        title: "Ceva",
-        director: "Cineva",
-        year: 2025
-    },
-    {
-        id: 2,
-        title: "Altceva",
-        director: "Tot Cineva",
-        year: 2013
-    }
-]
-const Movie = ({title, director, year}) => (
-    <div className={"p-6 bg-blue-300 rounded-xl"}>
-        <h1 className={"text-3xl"}>{title}</h1>
-        <h2 className={"text-xl"}>made by {director} and released in {year}</h2>
-    </div>
-)
+import { useEffect, useState } from "react";
+import Loading from "./Components/Loading.jsx";
 
-const MovieList = ({movies}) => {
+const Movie = ({ title, director, year }) => (
+  <div className={"p-7 border-slate-800 border-b-2 hover:bg-blue-400"}>
+    <h1 className={"text-3xl"}>{title}</h1>
+    <h2 className={"text-xl"}>
+      made by {director} and released in {year}
+    </h2>
+  </div>
+);
 
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("http://localhost:5070/api/movie");
+      console.log(result);
+      const data = await result.json();
+      console.log(data);
+      setMovies(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading)
     return (
-        <div className={"w-3/4 mx-auto my-0"}>
-            {movies.map((movie) => {
-                return (
-                    <Movie
-                        key={movie.id}
-                        title={movie.title}
-                        director={movie.director}
-                        year={movie.year}
-                    />
-                )
-            })}
-        </div>
-    )
-}
+      <div className={"flex align-middle justify-center p-16"}>
+        <Loading />
+      </div>
+    );
+
+  return (
+    <div className={"bg-blue-200 w-3/4 mx-auto my-0"}>
+      <div className={"border-b-4 border-slate-800 p-6 flex justify-center"}>
+        <h1 className={"text-5xl font-bold"}>All movies</h1>
+      </div>
+      {movies.map((movie) => {
+        return (
+          <Movie
+            key={movie.id}
+            title={movie.titlu}
+            director={movie.director}
+            year={movie.an}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 function App() {
-    return (
-        <>
-            <MovieList
-                movies={moviesJSON}
-            />
-        </>
-    )
+  return (
+    <>
+      <MovieList />
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Components/Loading.jsx
+++ frontend/src/Components/Loading.jsx
@@ -1,7 +1,7 @@
 function Loading() {
     return (
         <div
-            className="inline-block h-8 w-8 animate-spin rounded-full border-4
+            className="inline-block h-14 w-14 animate-spin rounded-full border-4
             border-solid border-current border-r-transparent align-[-0.125em]
             motion-reduce:animate-[spin_1.5s_linear_infinite]"
             role="status">
